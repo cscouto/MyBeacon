faa3cc801ccfd8ffa67ac8d2b0e7ad9a5c9724d5
Tiago Do Couto <cs.couto@outlook.com>
2017-09-05 10:47:56 -0300

    settings

--- My Beacon/ViewController.swift
+++ My Beacon/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    let beaconManager = ESTBeaconManager()
+    let beaconRegion = CLBeaconRegion(
+        proximityUUID: UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!,
+        identifier: "ranged region")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 3. Set the beacon manager's delegate
+        self.beaconManager.delegate = self
+        // 4. We need to request this authorization for every beacon manager
+        self.beaconManager.requestWhenInUseAuthorization()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.beaconManager.startRangingBeacons(in: self.beaconRegion)
+        self.beaconManager.startMonitoring(for: self.beaconRegion)
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.beaconManager.stopRangingBeacons(in: self.beaconRegion)
+        beaconManager.stopMonitoring(for: beaconRegion)
+    }
+    
 
+}
 
+extension ViewController: ESTBeaconManagerDelegate {
+    func beaconManager(_ manager: Any, didRangeBeacons beacons: [CLBeacon],
+                       in region: CLBeaconRegion) {
+        if let nearestBeacon = beacons.first {
+            print(nearestBeacon)
+        }
+    }
+    func beaconManager(_ manager: Any, didEnter region: CLBeaconRegion) {
+        print(region)
+    }
+    func beaconManager(_ manager: Any, didExitRegion region: CLBeaconRegion) {
+        print(region)
+    }
+    func beaconManager(_ manager: Any, didDetermineState state: CLRegionState, for region: CLBeaconRegion) {
+        print(region)
+    }
 }
 
